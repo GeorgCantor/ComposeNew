13485d8c37bc6568cfe00f23f84b092e608ec49d
GeorgCantor <cupsmana16>
2022-05-03 19:55:09 +0300

    Initial commit

---
+++ data/src/main/java/com/example/data/api/MovieApi.kt
@@ -0,0 +1,13 @@
+package com.example.data.api
+
+import com.example.domain.model.MovieList
+import retrofit2.Response
+import retrofit2.http.Query
+
+interface MovieApi {
+
+    suspend fun getPopularMovies(
+        @Query("api_key") apiKey: String,
+        @Query("page") page: Int = 1
+    ): Response<MovieList>
+}
