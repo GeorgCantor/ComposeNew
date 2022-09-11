e83217a68e4496ebc8f347542cac6b4f396df445
GeorgCantor <cupsmana16>
2022-09-11 17:37:49 +0300

    Add compose ui

---
+++ app/src/main/java/com/example/compose/presentation/navigation/NavGraph.kt
@@ -0,0 +1,25 @@
+package com.example.compose.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.compose.presentation.screen.home.HomeScreen
+
+@Composable
+fun NavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(route = Screen.Home.route) { HomeScreen(navController = navController) }
+        composable(
+            route = Screen.MovieDetails.route,
+            arguments = listOf(navArgument("movieId") { type = NavType.StringType })
+        ) {
+            it.arguments?.getString("movieId")?.let { MovieDetailsScreen(it, navController) }
+        }
+    }
+}
