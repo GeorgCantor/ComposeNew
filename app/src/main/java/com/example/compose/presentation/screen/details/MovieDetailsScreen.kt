4fb73262229e59e67da00a74fac02a397e3c392d
GeorgCantor <cupsmana16>
2022-09-18 16:02:55 +0300

    Add details screen

---
+++ app/src/main/java/com/example/compose/presentation/screen/details/MovieDetailsScreen.kt
@@ -0,0 +1,27 @@
+package com.example.compose.presentation.screen.details
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.compose.ui.theme.AppContentColor
+import com.example.compose.ui.theme.AppThemeColor
+
+@Composable
+fun MovieDetailsScreen(
+    movieId: String,
+    navController: NavController,
+    viewModel: MovieDetailsViewModel = hiltViewModel(),
+) {
+    viewModel.getMovieDetails(movieId = movieId.toInt())
+    val movie by viewModel.selectedMovie.collectAsState()
+    Scaffold(
+        topBar = { MovieDetailsTopBar(navController) },
+        contentColor = MaterialTheme.colors.AppContentColor,
+        backgroundColor = MaterialTheme.colors.AppThemeColor,
+        content = { movie?.let { MovieDetailsContent(it) } }
+    )
+}
