4061ff6d8f43e6f57eb56e29950faab37100809f
GeorgCantor <cupsman1986@gmail.com>
2023-03-19 14:38:15 +0300

    Change content

--- app/src/main/java/com/example/compose/presentation/screen/home/NewsListContent.kt
+++ app/src/main/java/com/example/compose/presentation/screen/home/NewsListContent.kt
@@ -4,14 +4,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.paging.compose.LazyPagingItems
@@ -33,9 +37,12 @@ fun NewsListContent(allNews: LazyPagingItems<New>, navController: NavHostControl
 }
 
 @Composable
-fun NewsItem(aNew: New, navController: NavHostController) {
+fun NewsItem(aNew: New, navController: NavHostController?) {
     Card(
-        modifier = Modifier.padding(top = 8.dp).height(180.dp).fillMaxWidth(),
+        modifier = Modifier
+            .padding(top = 8.dp)
+            .height(180.dp)
+            .fillMaxWidth(),
         elevation = 4.dp,
         backgroundColor = MaterialTheme.colors.ItemBackgroundColor
     ) {
@@ -44,34 +51,65 @@ fun NewsItem(aNew: New, navController: NavHostController) {
                 .height(IntrinsicSize.Max)
                 .fillMaxWidth()
                 .clickable {
-                    navController.navigate(route = Screen.NewsDetails.passId(aNew.id.toString()))
+                    navController?.navigate(route = Screen.NewsDetails.passId(aNew.id.toString()))
                 },
             verticalAlignment = Alignment.CenterVertically
         ) {
             aNew.posterPath?.let {
                 Image(
-                    modifier = Modifier.padding(end = 4.dp,).width(120.dp),
+                    modifier = Modifier
+                        .padding(start = 4.dp)
+                        .height(160.dp)
+                        .width(120.dp)
+                        .clip(RoundedCornerShape(4.dp)),
                     painter = rememberImagePainter(
                         data = aNew.posterPath, builder = {
                             crossfade(true)
                             scale(Scale.FILL)
                         }),
                     contentDescription = null,
-                    contentScale = ContentScale.Fit
+                    contentScale = ContentScale.Crop
                 )
             }
-            Column(Modifier.height(IntrinsicSize.Max).padding(end = 2.dp)) {
-                aNew.title?.let { Text(text = it, style = MaterialTheme.typography.body1) }
+            Column(
+                Modifier
+                    .height(IntrinsicSize.Max)
+                    .padding(horizontal = 8.dp, vertical = 10.dp)
+                    .align(Alignment.Top)
+            ) {
+                aNew.title?.let {
+                    Text(
+                        text = it,
+                        maxLines = 3,
+                        style = MaterialTheme.typography.body1
+                    )
+                }
                 Spacer(modifier = Modifier.height(4.dp))
                 aNew.overview?.let {
                     Text(
                         text = it,
                         style = MaterialTheme.typography.body2,
-                        maxLines = 4,
-                        overflow = TextOverflow.Ellipsis
+                        maxLines = 8,
+                        overflow = TextOverflow.Ellipsis,
+                        color = Color.Gray
                     )
                 }
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun NewsItemPreview() {
+    NewsItem(
+        aNew = New(
+            pk = 0,
+            id = 1,
+            overview = "To set rounded corners for Image in Android Compose, apply clip modifier with RoundedCornerShape. Pass required corner radius in dp, float, or int (percentage) to RoundedCornerShape().",
+            posterPath = "",
+            title = "Title",
+            releaseDate = "20.09.2020",
+        ), navController = null
+    )
 }
